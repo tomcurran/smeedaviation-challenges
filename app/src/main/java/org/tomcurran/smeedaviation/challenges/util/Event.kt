3f03b6de7917dc8a99cd8791c89aee3398880cd2
Tom Curran <tom@tomcurran.org>
2020-06-21 11:10:27 +0100

    Added link to original source of Event & EventObserver

--- app/src/main/java/org/tomcurran/smeedaviation/challenges/util/Event.kt
+++ app/src/main/java/org/tomcurran/smeedaviation/challenges/util/Event.kt
@@ -1,25 +1,10 @@
-/*
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.tomcurran.smeedaviation.challenges.util
 
 import androidx.lifecycle.Observer
 
 /**
  * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ * Original: https://gist.github.com/JoseAlcerreca/5b661f1800e1e654f07cc54fe87441af
  */
 open class Event<out T>(private val content: T) {
 
@@ -49,6 +34,7 @@ open class Event<out T>(private val content: T) {
  * already been handled.
  *
  * [onEventUnhandledContent] is *only* called if the [Event]'s contents has not been handled.
+ * Original: https://gist.github.com/JoseAlcerreca/e0bba240d9b3cffa258777f12e5c0ae9
  */
 class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
     override fun onChanged(event: Event<T>?) {
